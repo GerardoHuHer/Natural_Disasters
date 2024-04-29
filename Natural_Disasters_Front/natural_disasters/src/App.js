720bf4b4168f86edeb083dc85980905971593a2c
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-04-29 10:48:58 -0600

    Second Component

--- Natural_Disasters_Front/natural_disasters/src/App.js
+++ Natural_Disasters_Front/natural_disasters/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import DisasterStatsComponent from './Componente_principal';
+import DesastresPorPais from './DesastrePais';
 
 function App() {
   return (
     <div className="App">
       <body>
         <DisasterStatsComponent/>
+        <DesastresPorPais/>
       </body>
     </div>
   );
